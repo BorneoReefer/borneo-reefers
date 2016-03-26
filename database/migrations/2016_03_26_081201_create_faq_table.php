710d54d15373b16029db04327849b811f62e5c91
Timur Lavrenti Kiyivinski <deathseclipse@gmail.com>
2016-03-26 16:17:10 +0800

    [WIP] Database models and migrations

---
+++ database/migrations/2016_03_26_081201_create_faq_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFaqTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('faq', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('faq');
+    }
+}
